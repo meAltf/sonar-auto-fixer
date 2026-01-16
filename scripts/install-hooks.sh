c225bcfdac3431088e23a0fdf5bfb93bf7e0d924
meAltf <alataf.dce.cs1@gmail.com>
2026-01-16 23:01:14 +0530

    working sonar fixes configuration

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -8,7 +8,7 @@ mvn rewrite:run
 
 git diff
 read -p "Review changes above. Continue commit? (Yes/No): " ans
-[ "$ans" != "y" ] && exit 1
+[ "$ans" != "yes" ] && exit 1
 EOF
 
 chmod +x .git/hooks/pre-commit
